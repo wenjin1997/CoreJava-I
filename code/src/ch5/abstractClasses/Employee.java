fb77c0bcef9dd88fab79fe9602f5168fb0be4164
wenjin1997 <wenjin1997@126.com>
2022-02-14 10:35:47 +0800

    增加 第5章代码

---
+++ code/src/ch5/abstractClasses/Employee.java
@@ -0,0 +1,37 @@
+package ch5.abstractClasses;
+
+import java.time.LocalDate;
+
+/**
+ * @author 文进
+ * @version 1.0
+ */
+public class Employee extends Person{
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        super(name);
+        this.salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    @Override
+    public String getDescription() {
+        // String.format方法创建一个格式化的字符串，而不打印输出
+        return String.format("an employee with a salary of $%.2f", salary);
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
