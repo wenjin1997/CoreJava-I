59b57c7925e52feb666134837e5cb6080f1d178a
wenjin1997 <wenjin1997@126.com>
2023-06-04 18:16:24 +0800

    update

---
+++ code/src/ch12/threads/ThreadTest.java
@@ -0,0 +1,19 @@
+package ch12.threads;
+
+/**
+ * @author 文进
+ * @version 1.0
+ * 书P554 程序清单12-1
+ */
+public class ThreadTest {
+    public static final int DELAY = 10;
+    public static final int STEPS = 100;
+    public static final double MAX_AMOUNT = 1000;
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
